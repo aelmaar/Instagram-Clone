aa6efb1f3298ea5538fb8c1aaecae450794879bb
Anouar <anoirelmaaroufi@gmail.com>
2020-10-24 14:23:28 +0100

    upload firebase file you should make your own config

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyBidT6iSA4I0qe2WqyniqCyU5i_sb3uAos",
-	authDomain: "instagram-demo-clone.firebaseapp.com",
-	databaseURL: "https://instagram-demo-clone.firebaseio.com",
-	projectId: "instagram-demo-clone",
-	storageBucket: "instagram-demo-clone.appspot.com",
-	messagingSenderId: "697259998231",
-	appId: "1:697259998231:web:b65ae22b509dc405d5b052",
-	measurementId: "G-E1TPESG97N" 
+	// your config her
 })
 
 const db = firebaseApp.firestore();
